4419b557636855bec92b985a1a21744a95541568
Ardian <PC@DESKTOP-DK0RG40>
2025-06-08 23:09:02 +0200

    Added some fixes for the endpoints

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,7 @@
 export default {
   providers: [
     {
-      domain: "https://native-raven-73.clerk.accounts.dev/",
+      domain: "https://relaxing-caribou-20.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ]
